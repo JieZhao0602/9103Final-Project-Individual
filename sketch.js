31f489517f257d981ae3a88523768db2eb3dd755
JieZhao0602 <jiezhao010602@gmail.com>
2023-11-05 17:57:03 +0800

    initialization and set up

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+let circles = [];//initialization and set up
+const maxRadius = 40;
+const bigCircleRadius = 70;
+let bgColor = [44, 61, 100];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB);
+  makeCircles();
 }
 
-function draw() {
-  background(220);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  makeCircles();
 }
